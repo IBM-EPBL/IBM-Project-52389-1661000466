4ca7ab8d383ff96bff34ebcd07ce098be900b7e7
Girijasrinivasan2002 <113118568+Girijasrinivasan2002@users.noreply.github.com>
2022-10-15 10:25:49 +0530

    deleted

--- ASSIGNMENT/Team Member 4-Rajkumar A/Assignment-2/flask-with-sqlite/sqlite_db_setup.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('studentss.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE customers (email VARCHAR, password VARCHAR)')
-print("Table created successfully")
-conn.close()
